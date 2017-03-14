7c6ad6779f189573af7e710dd713d90e5ae78e23
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-14 13:55:28 -0700

    Update and delete

--- src/app/post/post-show.component.ts
+++ src/app/post/post-show.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
 import { Http } from '@angular/http';
 import { Post } from './post';
 import { PostService } from './post.service';
@@ -12,16 +13,21 @@ import { PostService } from './post.service';
 export class PostShowComponent implements OnInit {
   id: number;
   routeId: any;
+  errorMessage: any;
+  returnUrl: string;
+  editBtnClicked: boolean = false;
 
   constructor(
     private http: Http,
     private route: ActivatedRoute,
-    private postService: PostService
+    private postService: PostService,
+    private router: Router
   ) {}
 
   @Input() post: Post;
 
   ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/posts';
     this.routeId = this.route.params.subscribe(
       params => {
         this.id = +params['id']
@@ -31,5 +37,28 @@ export class PostShowComponent implements OnInit {
       .flatMap((params: Params) =>
         this.postService.getPost(+params['id']));
     postRequest.subscribe(response => this.post = response.json());
+  }
+
+  update(post: Post) {
+    this.editBtnClicked = true;
+    this.postService.updatePost(post)
+      .subscribe(data => { return true},
+        error => {
+          console.log("Error editing post");
+          return Observable.throw(error);
+        });
+  }
+
+  delete(post: Post) {
+    this.postService.deletePost(this.post.id)
+      .subscribe(data => {
+        this.router.navigate([this.returnUrl]);
+      },
+        error => this.errorMessage = error);
+  }
+
+  onUpdateClicked() {
+    this.router.navigate([this.returnUrl]);
+    this.editBtnClicked = false;
   }
 }
